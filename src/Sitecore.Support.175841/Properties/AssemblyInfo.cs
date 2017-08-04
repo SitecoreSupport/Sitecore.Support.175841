364ab098b09c1822438d730cac090f95176db267
Ivan Sheyenko <ivs@sitecore.net>
2017-08-04 10:35:14 +0300

    patch created

---
+++ src/Sitecore.Support.175841/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.175841")]
+[assembly: AssemblyProduct("Sitecore.Support.175841")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.175841/ProtectExternalLink.cs
@@ -0,0 +1,39 @@
+﻿using Sitecore.Configuration;
+using Sitecore.Shell.Controls.RichTextEditor.Pipelines.SaveRichTextContent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace Sitecore.Support.Shell.Controls.RichTextEditor.Pipelines.SaveRichTextContent
+{
+  public class ProtectExternalLink: Sitecore.Shell.Controls.RichTextEditor.Pipelines.SaveRichTextContent.ProtectExternalLink
+  {
+    public new void Process(SaveRichTextContentArgs args)
+    {
+      if (string.IsNullOrEmpty(args.Content))
+      {
+        return;
+      }
+      if (!Settings.ProtectExternalLinksWithBlankTarget)
+      {
+        return;
+      }
+      MatchCollection aTagMatchCollection = this.GetATagMatchCollection(args);
+      foreach (Match match in aTagMatchCollection)
+      {
+        if (match.Success)
+        {
+          string value = match.Value;
+          if (!string.IsNullOrEmpty(value) && value.Contains("_blank") && !this.IsInternalLink(value))
+          {
+            string protectedLink = value.Insert(2, " rel=\"noopener noreferrer\" ");
+            args.Content = args.Content.Replace(value, protectedLink);
+            //args.Content = this.GetProtectedHtml(args, args.Content.IndexOf(" ", match.Index, StringComparison.InvariantCultureIgnoreCase), match.Length);
+          }
+        }
+      }
+    }
+  }
+}
